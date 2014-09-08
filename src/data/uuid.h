05513933339f920ad3508bf2ec1059fb62528c5d
Joe Fredette <jfredett@gmail.com>
2014-09-08 01:42:56 -0400

    wrap uuid in a structure for better type safety

--- src/data/uuid.h
+++ src/data/uuid.h
@@ -1,1 +1,10 @@
-char* get_uuid(void);
+#include <stdbool.h>
+
+typedef struct _uuid {
+  // uuid_unparse generates a 37 byte string w/ the \0 at the end
+  char* string;
+} uuid;
+
+uuid* get_uuid(void);
+bool uuid_cmp(uuid* a, uuid* b);
+bool uuid_null(uuid* a);
